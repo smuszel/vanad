573414694b5b6a92824b1091bcf2a2ea8896eef2
Stan Muszel <stanislaw.muszel@gmail.com>
2019-05-12 10:34:54 +0200

    use better format

--- test2/run.js
+++ test2/run.js
@@ -1,32 +1,27 @@
 const states = require('./states');
-const tests = require('./tests');
+const tests = require('./test.spec');
 const ppr = require('puppeteer');
 const { cyan, green, red } = require('chalk').default;
 const browser = ppr.launch();
 
-const lift = xs => {
-    return Array.isArray(xs) ? xs : Array.of(xs);
-}
 
-const run = async label => {
-    const state = states[label](browser);
-    const testSelectors = lift(tests[label]);
-    // console.log(testSelectors);
+const run = async ({ resolver, description, selectors }) => {
+    const state = resolver(await browser);
     const page = (await state).page;
-    const selPromises = testSelectors.map(sel => {
+    const selPromises = selectors.map(sel => {
         return page.waitForSelector(sel, { timeout: 3000 })
             .then(() => ({ err: false, sel }))
             .catch(err => ({ err, sel }));
     });
     const stateResults = await Promise.all(selPromises);
 
-    return { label, stateResults };
+    return { description, stateResults };
 };
 
-Promise.all(Object.keys(tests).map(run))
+Promise.all(tests.map(run))
     .then(suiteResults => {
         const formattedResults = suiteResults.map(sr => {
-            const header = `During ${cyan(sr.label)} I should see:`;
+            const header = `${cyan(sr.description)} I should see:`;
             const units = sr.stateResults.map(r => {
                 const txt = `* ${r.sel}`
                 return { txt: r.err ? red(txt) : green(txt), err: r.err };
