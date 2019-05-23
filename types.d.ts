f9b402f83965239c95c05af9958fdbea3b39b1a5
Stan Muszel <stanislaw.muszel@gmail.com>
2019-05-23 19:40:52 +0200

    clean up

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,8 @@
+declare type Browser = import('puppeteer').Browser
+declare type BrowserContext = import('puppeteer').BrowserContext
+declare type Page = import('puppeteer').Page
 declare type Dict<K, T> = { [k in K]: T }
 declare type OptDict<K, T> = { [k in K]?: T }
-declare type Browser = import('puppeteer').Browser
-declare type Page = import('puppeteer').Page
 declare type VerbosityLevel = 'debug' | 'silent' | 'bare'
 declare type BrowserMode = 'headless' | 'remote' | 'preview'
 declare type MessageType = keyof typeof import('./src/messages')
@@ -14,51 +15,26 @@ declare type ArgVars = {
     pattern: string
 }
 
-declare type StdoutRender = (value: any) => void
+interface NodeRequire {
+    (id: PathTestGenerator): TestGenerator<any>
+}
 
+declare type StdoutRender = (value: any) => void
+declare interface PathTestGenerator { }
 declare type Job = {
     data: any
-    path: string
+    path: PathTestGenerator
     name: string
 }
 
 declare type Message = {
     type: MessageType
-    value?: any
+    data: {
+        job?: Job,
+        name?: string
+    }
 }
-
-declare type Chanel = (msg: MessageType) => void
+declare type TestGenerator<T> = ({ context: BrowserContext, data: T }) => AsyncIterableIterator<Step>
+declare type Chanel = (msg: Message) => void
 declare type Worker = (chanel: Chanel, job: Job) => Promise<void>
-declare type JobExecution = (job: Job) => Promise<void>
-declare type Query = () => Promise<QueryFailure?>
-declare type QueryFactory<A, B> = (a: A, b: B) => Query
-declare interface Step {
-    label: string,
-    expect?: Query
-}
-declare type TestFactory<T> = (browser: Browser, data: T) => TestGen
-declare type TestGen = () => AsyncIterableIterator<Step>
-
-//
-
-declare type Middleware = { [k in MessageType]?: (...args: any[]) => Promise<number> }
-
-//
-
-declare type RunArgv = (argv: ArgVars) => Stream<ExecutionState>
-
-declare type ExcutionState = {
-    messages: Message[]
-
-    runningJobs: Jobs[]
-    availableWorkers: Worker[],
-    pendingJobs: Jobs[]
-    runningTests: Test[]
-    passedTests: Test[]
-    failedTests: Test[]
-    pendingTests: Test[]
-    runningSteps: Step[]
-    passedSteps: Step[]
-    failedSteps: Step[]
-    pendingSteps: Step[]
-}
+declare type Step = boolean
