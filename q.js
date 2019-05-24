1fcf52729bc6d79997ddb85c90a97bd2434d3e71
Stan Muszel <stanislaw.muszel@gmail.com>
2019-05-24 23:10:19 +0200

    rm experimets

--- q.js
+++
@@ -1,9 +0,0 @@
-const logUpdate = require('log-update');
-
-const frames = ['-', '\\', '|', '/'];
-let i = 0;
-
-setInterval(() => {
-    const frame = frames[(i = ++i % frames.length)];
-    logUpdate(frame);
-}, 80);
