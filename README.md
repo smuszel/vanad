75ebdda6c14a2154730e9bd641ce0be101cfe890
smuszel <stanislaw.muszel@gmail.com>
2019-06-09 18:18:25 +0200

    prep for release

--- README.md
+++
@@ -1,5 +0,0 @@
-### Note it is still experimental and in early development
-
-* investigate use of plugins
-* parallel tests
-* monorepo with ext
--- bin/vanad.js
+++ bin/vanad.js
@@ -5,20 +5,23 @@ const yargs = require('yargs');
 const extensions = ['.js', '.jsx', '.es6', '.es', '.mjs', '.ts', '.tsx'];
 
 const main = () => {
-    const { pattern, cwd, babel } = yargs
+    const { pattern, cwd, babel, verbosity } = yargs
         .option('cwd', { default: process.cwd() })
         .option('babel', { alias: 'b' })
+        .option('verbosity', { alias: 'v', default: 'basic', choices: ['basic', 'full'] })
         .option('pattern', {
             alias: 'p',
-            default: './**/*.spec.js',
+            default: './**/*.spec.*',
         }).argv;
 
     if (babel) {
         require('@babel/register')({ extensions });
     }
 
+    global['verbosity'] = verbosity;
+
     glob(pattern, { cwd }, (_, testFiles) => {
-        testFiles.forEach((tf, ix) => {
+        testFiles.forEach(tf => {
             const fullPath = path.join(cwd, tf);
             require(fullPath);
         });
